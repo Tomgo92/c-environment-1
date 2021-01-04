a5bf1ed051bf5ddc5ea4bc3099b3f625f10ef184
Matthieu Roman <mcfly@pop-os.localdomain>
2021-01-04 15:11:44 +0100

    Update

--- main.c
+++ main.c
@@ -1,11 +1,22 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(){
-int i = 1;
-
-for(i = 1; i <= 10; i++)
-printf("%d\n", i);
-
-return(0);
-
+int main(int argc, char *argv[])
+{
+    if(argc<2)
+    {
+        for(int i=1;i<=10;i++)
+        {
+            printf("%d\n",i);
+        }
+    }
+    else
+    {
+        int N=atoi(argv[1]);
+        for(int i=1;i<=N;i++)
+        {
+            printf("%d\n",i);
+        }
+    }
+    return 0;
 }
